4b836aacf8f2a792572f863dad104202a3c22004
DevSupport <46906875+DevSupDeveloper@users.noreply.github.com>
2019-02-21 00:32:10 +0100

    Create index.php

---
+++ cgu/index.php
@@ -0,0 +1,101 @@
+<html>
+	<head>
+		<title>DevSupport - CGU</title>
+		<link rel="icon" href="/style/icon.png">
+		<link type="text/css" rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"  media="all"/>
+	    <!-- Fork Awesome V1 -->
+	    <link  type="text/css" rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/fork-awesome.min.css">
+	    <!-- Stylesheep custom -->
+	    <link type="text/css" rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/custom.css">
+		<style type="text/css">
+			@font-face {
+	  			font-family: 'calibri'; /*a name to be used later*/
+	   			src: url('/style/calibri.ttf'); /*URL to font*/
+			}
+			@font-face {
+			    font-family: 'space'; /*a name to be used later*/
+			    src: url('/style/space.ttf'); /*URL to font*/
+			}
+		</style>
+	</head>
+	<body>
+		<div class="jumbotron">
+  		<h1 class="titled">Conditions Générales d'Utilisation</h1>
+  		<p class="lead">Mentions légales, Cookies, Propriété intellectuelle, Règles d’usage d’Internet.</p>
+		</div>
+		<div class="container">
+		    <div class="row">
+		        <div class="col-sm-12 col-md-9">
+		            <div class="card box-shadow">
+		                <div class="card-body">
+		                    <div class="row">
+		                        <div class="col">
+		                            <h4 class="card-title default-color-text source-code-pro"><i class="fa fa-id-card-o"></i> Mentions légales</h4>
+		                        </div>
+		                    </div>
+		                    <p class="card-text">
+		                      Propriétaire, Directeur de publication : <b>Quentinium</b> - <code>contact[at]devsupport.fr</code><br>
+				      Webmaster : <b>Zeldown</b> - <code>zeldown[at]devsupport.fr</code><br>
+		                      Date d'enregistrement du nom de domaine : <b>09/12/2018</b><br>
+		                      Hébergeur :  <b>StayHost</b> - 4 quai Jean Moulin - 69001 Lyon France<br>
+		                      Pays : <b>France</b><br>
+		                    </p>
+		                </div>
+		            </div>
+		            <div class="card box-shadow">
+		                <div class="card-body">
+		                    <div class="row">
+		                        <div class="col">
+		                            <h4 class="card-title default-color-text source-code-pro"><i class="fa fa-exclamation-circle"></i> Cookies</h4>
+		                        </div>
+		                    </div>
+		                    <p class="card-text">
+		                        Le site Web utilise les cookies provenant de <b>G. Analytics</b> pour compter le nombre de visites.<br>
+		                        Ce sont des cookies qui permettent la navigation et fournissent un service requis par l'utilisateur. Ils ne sont pas utilisés à des fins ultérieures et sont installés directement par le propriétaire du site web.<br>
+		                        Les cookies analystiques sont utilisés pour recueillir des statistiques anonymes sur l'utilisation du site par les utilisateurs (par exemple, le nombre de visiteurs, les liens de provenance, les mots clés utilisés pour trouver le site sur les moteurs de recherche). Ils peuvent être envoyés par le même site ou par d'autres sites (tiers).
+		                    </p>
+		                </div>
+		            </div>
+		            <div class="card box-shadow">
+		                <div class="card-body">
+		                    <div class="row">
+		                        <div class="col">
+		                            <h4 class="card-title default-color-text source-code-pro"><i class="fa fa-gavel"></i> Propriété intellectuelle</h4>
+		                        </div>
+		                    </div>
+		                    <p class="card-text">
+		                        La structure générale du site "devsupport.fr" et ses sous domaines, ainsi que les textes, graphiques, images, sons, feuilles de styles, systèmes informatisés, codes et vidéos la composant, sont la propriété du propriétaire ou de ses partenaires.<br><br>
+		                        Toute représentation et/ou reproduction et/ou exploitation partielle ou totale des contenus et services proposés par le site "devsupport.fr" , par quelque procédé que ce soit, sans l’autorisation préalable et par écrit de "devsupport.fr" et/ou de ses partenaires est strictement interdite et serait susceptible de constituer une contrefaçon au sens des articles L 335-2 et suivants du Code de la propriété intellectuelle.<br>
+		                        <div class="alert alert-danger text-center" role="alert">
+		                            Aucune reproduction, même partielle prévue à l’article L.122-5 du Code de la propriété intellectuelle, ne peut être faite de ce site sans l’autorisation du directeur de publication.
+		                        </div>
+		                        Les informations et/ou documents figurant sur ce site et/ou accessibles par ce site proviennent de sources considérées comme étant fiables. Toutefois, ces informations et/ou documents sont susceptibles de contenir des inexactitudes techniques et des erreurs typographiques.
+		                    </p>
+		                </div>
+		            </div>
+		            <div class="card box-shadow">
+		                <div class="card-body">
+		                    <div class="row">
+		                        <div class="col">
+		                            <h4 class="card-title default-color-text source-code-pro"><i class="fa fa-align-left"></i> Règles d’usage d’Internet</h4>
+		                        </div>
+		                    </div>
+		                    <p class="crad-text">L’utilisateur déclare accepter les caractéristiques et les limites d’Internet, et notamment reconnaît que :</p>
+		                    <ul class="list-group">
+		                        <li class="list-group-item"> Le propriétaire n’assume aucune responsabilité sur les services accessibles par Internet et n’exerce aucun contrôle de quelque forme que ce soit sur la nature et les caractéristiques des données qui pourraient transiter par l’intermédiaire de son centre serveur. </li>
+		                        <li class="list-group-item">L’utilisateur reconnaît que les données circulant sur Internet ne sont pas protégées notamment contre les détournements éventuels. La présence du logo institut une présomption simple de validité.
+		                            La communication de toute information jugée par l’utilisateur de nature sensible ou confidentielle se fait à ses risques et périls. </li>
+		                        <li class="list-group-item"> L’utilisateur reconnaît que Le propriétaire ne dispose d’aucun moyen de contrôle sur le contenu des services accessibles sur Internet. </li>
+		                        <li class="list-group-item"> L’utilisateur reconnaît que les données circulant sur Internet peuvent être réglementées en termes d’usage ou être protégées par un droit de propriété. </li>
+		                        <li class="list-group-item"> L’utilisateur est seul responsable de l’usage des données qu’il consulte, interroge et transfère sur Internet. </li>
+		                    </ul>
+		                </div>
+		            </div>
+		        </div>
+		    </div>
+		</div>
+		<?php
+		include '../includes/analyse.html';
+ 		?>
+	</body>
+</html>
